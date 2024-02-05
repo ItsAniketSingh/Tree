c7ef75d6cc83b150fa466c16925b042a239c81d7
Aniket Singh <95512260+ItsAniketSingh@users.noreply.github.com>
2024-02-05 19:48:39 +0530

    Update BinaryTreeInorderTraversal.java

--- BinaryTreeInorderTraversal.java
+++ BinaryTreeInorderTraversal.java
@@ -7,15 +7,11 @@ import javax.swing.tree.TreeNode;
     94. Binary Tree Inorder Traversal
     Given the root of a binary tree, return the inorder traversal of its nodes' values.
 
- 
-
 Example 1:
-
-
 Input: root = [1,null,2,3]
 Output: [1,3,2]
-Example 2:
 
+Example 2:
 Input: root = []
 Output: []
 Example 3:
