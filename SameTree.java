997f8a97e53b6617a472adaee0804db7a03db5bd
Aniket Singh <95512260+ItsAniketSingh@users.noreply.github.com>
2024-03-03 19:35:55 +0530

    Update SameTree.java

--- SameTree.java
+++ SameTree.java
@@ -7,18 +7,14 @@
  
 
 Example 1:
-
-
 Input: p = [1,2,3], q = [1,2,3]
 Output: true
+
 Example 2:
-
-
 Input: p = [1,2], q = [1,null,2]
 Output: false
+
 Example 3:
-
-
 Input: p = [1,2,1], q = [1,1,2]
 Output: false
  */
