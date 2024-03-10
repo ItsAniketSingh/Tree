1018ee9e6fe1db7242c66e89a363d9a266315f6c
Aniket Singh <95512260+ItsAniketSingh@users.noreply.github.com>
2024-03-10 14:17:06 +0530

    Update EvenOddTree.java

--- EvenOddTree.java
+++ EvenOddTree.java
@@ -6,7 +6,6 @@
     For every odd-indexed level, all nodes at the level have even integer values in strictly decreasing order (from left to right).
     Given the root of a binary tree, return true if the binary tree is Even-Odd, otherwise return false.
 
- 
 
 Example 1:
 Input: root = [1,10,4,3,null,7,9,12,8,6,null,null,2]
